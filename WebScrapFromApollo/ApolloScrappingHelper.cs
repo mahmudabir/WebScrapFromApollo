c27887c90460120d60900c82dc6f8dc36798f681
Abir Mahmud <amabirmahmud@gmail.com>
2024-01-13 23:54:27 +0600

    init

---
+++ WebScrapFromApollo/ApolloScrappingHelper.cs
@@ -0,0 +1,41 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace WebScrapFromApollo
+{
+    public class ApolloScrappingHelper : IScrappingHelper
+    {
+
+        public void StartScrapping()
+        {
+            var baseUrl = "https://app.apollo.io/#";
+
+            string loginUrl = $"{baseUrl}/login";
+            string peopleUrl = $"{baseUrl}/people";
+
+            using IWebDriver driver = new ChromeDriver();
+
+            driver.Navigate().GoToUrl(loginUrl);
+
+            Thread.Sleep(5000);
+
+            IWebElement emailField = driver.FindElement(By.XPath("/html/body/div[2]/div/div[2]/div[1]/div/div[2]/div/div[2]/div/form/div[5]/div/div/input"));
+            IWebElement passwordField = driver.FindElement(By.XPath("//*[@id=\"current-password\"]"));
+            IWebElement keepLoggedinField = driver.FindElement(By.XPath("//*[@id=\"provider-mounter\"]/div/div[2]/div[1]/div/div[2]/div/div[2]/div/form/div[8]/div/div/div/label/div/i"));
+            IWebElement loginButton = driver.FindElement(By.XPath("//*[@id=\"provider-mounter\"]/div/div[2]/div[1]/div/div[2]/div/div[2]/div/form/div[7]/button"));
+
+            emailField.SendKeys("summa@b2bleadshouse.com");
+            passwordField.SendKeys("Carel56/*a5f8ef");
+            //keepLoggedinField.Click();
+            loginButton.Click();
+
+            Thread.Sleep(5000);
+
+
+            driver.Navigate().GoToUrl(peopleUrl);
+
+
+            //driver.Quit();
+        }
+    }
+}
---
+++ WebScrapFromApollo/Program.cs
@@ -0,0 +1,62 @@
+﻿using HtmlAgilityPack;
+
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+using PuppeteerSharp;
+
+using System.Xml.Linq;
+
+namespace WebScrapFromApollo
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+
+            var baseUrl = "https://app.apollo.io/#";
+
+            string loginUrl = $"{baseUrl}/login";
+            string peopleUrl = $"{baseUrl}/people";
+
+            // download the browser executable
+            await new BrowserFetcher().DownloadAsync();
+
+            // browser execution configs
+            var launchOptions = new LaunchOptions
+            {
+                Headless = false, // = false for testing
+            };
+
+            // open a new page in the controlled browser
+            using (var browser = await Puppeteer.LaunchAsync(launchOptions))
+            using (var page = await browser.NewPageAsync())
+            {
+                // visit the target page
+                await page.GoToAsync(loginUrl);
+
+                var emailField = await page.XPathAsync("/html/body/div[2]/div/div[2]/div[1]/div/div[2]/div/div[2]/div/form/div[5]/div/div/input");
+                await emailField.FirstOrDefault().TypeAsync("summa@b2bleadshouse.com");
+
+                var passwordField = await page.XPathAsync("//*[@id=\"current-password\"]");
+                await passwordField.FirstOrDefault().TypeAsync("Carel56/*a5f8ef");
+
+                var keepLoggedinField = await page.XPathAsync("//*[@id=\"provider-mounter\"]/div/div[2]/div[1]/div/div[2]/div/div[2]/div/form/div[8]/div/div/div/label/div/i");
+                var loginButton = await page.XPathAsync("//*[@id=\"provider-mounter\"]/div/div[2]/div[1]/div/div[2]/div/div[2]/div/form/div[7]/button");
+
+                await loginButton.FirstOrDefault().ClickAsync();
+
+
+
+                await page.GoToAsync(peopleUrl);
+
+
+
+                // retrieve the HTML source code and log it
+                var html = await page.GetContentAsync();
+                Console.WriteLine(html);
+            }
+
+        }
+    }
+}
